8bf51bcf86ef24c3f703fcafc10c3ffe1171b1cc
Kenny Umobi <122109136+Kenny-jnr@users.noreply.github.com>
2024-03-12 03:29:28 +0000

    implemented using external data

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,15 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
+
+  eleventyConfig.addShortcode("getImage", function() {
+    return `
+    <div x-data="{picture: $fetchjson('https://dummyjson.com/products/1', jsonItem='thumbnail')}">
+      <img alt="image1" x-bind:src="picture">
+    </div>
+    <div x-data>
+      <span x-text="await $fetchjson('https://dummyjson.com/products/1', jsonItem='category')"></span>
+    </div>
+    `
+  })
 };
